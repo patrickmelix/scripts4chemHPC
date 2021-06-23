2426ad1c6a018b1decc452fc8cc6942cdb6466cb
Patrick Melix <patrick.melix@uni-leipzig.de>
2021-06-23 11:51:22 +0200

    ORCA: fix combine script for numeric filenames

--- ORCA/combine-trajectory.sh
+++ ORCA/combine-trajectory.sh
@@ -1,2 +1,20 @@
-#!/bin/bash
-cat $(find . -name "*_trj.xyz" | sort -V) > traj.xyz
+#!/usr/bin/env python3
+from ase import io
+from natsort import natsorted
+import glob, os
+
+files = natsorted(glob.glob('**/*_trj.xyz', recursive=True))
+
+#if first entry is not in a subdir, it should be the last
+if not '/' in files[0]:
+   tmp = files[0]
+   del files[0]
+   files.append(tmp)
+
+with open('traj.xyz', 'w') as out:
+    for f in files:
+        print("Adding {}: ".format(f), end='')
+        frames = io.read(f, index=slice(0,None))
+        print("{} frames...".format(len(frames)))
+        io.write(out, frames, format='extxyz', append=True)
+print('...Done!')
