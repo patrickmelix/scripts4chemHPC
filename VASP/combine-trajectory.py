51900f7423ac1e41834721e5a24a85b10683c99a
Patrick Melix <patrick.melix@uni-leipzig.de>
2022-06-16 09:57:48 +0200

    VASP combine traj update to ignore non-digit subfolders

--- VASP/combine-trajectory.py
+++ VASP/combine-trajectory.py
@@ -7,6 +7,10 @@ files = natsorted(glob.glob('**/vasprun.xml', recursive=True))
 
 with open('traj.xyz', 'w') as out:
     for f in files:
+        folder = os.path.dirname(os.path.abspath(f))
+        if (not os.path.basename(folder).isdigit()) and (not os.path.realpath(folder) == os.path.realpath(os.getcwd())):
+            print("Not using {}".format(folder))
+            continue
         print("Adding {}: ".format(f), end='')
         frames = io.read(f, index=slice(0,None))
         print("{} frames...".format(len(frames)))
